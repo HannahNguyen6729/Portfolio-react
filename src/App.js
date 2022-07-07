855bea345764ba6a7da6e1a403db244106182576
HannahNguyen6729 <81440768+HannahNguyen6729@users.noreply.github.com>
2022-07-07 21:23:50 +0300

    add recentProject component

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import Home from "./components/Home/Home";
 import MainProjects from "./components/MainProjects/MainProjects";
 import OtherProjects from "./components/OtherProjects/OtherProjects";
 import Skills from "./components/Skills/Skills";
+import RecentProject from "./components/MainProjects/RecentProject";
 function App() {
   return (
     <div>
       <NavBar />
       <Home />
       <About />
+      <RecentProject/>
       <MainProjects />
       <OtherProjects />
       <Skills />
